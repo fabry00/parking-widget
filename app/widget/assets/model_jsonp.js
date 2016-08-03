d80bf92b7e233e62907604b45b26f338b52bba5f
fabry <fabry_00@hotmail.com>
2016-08-03 18:29:03 +0200

    added events, map placeolder

--- app/widget/assets/model_jsonp.js
+++ app/widget/assets/model_jsonp.js
@@ -1,1 +1,61 @@
-callback({"vendor":{"name":"ParkinGo","departureAirport":"Bergamo Orio al Serio","map":"http://lorempixel.com/370/390","features":["Parcheggio ParkinGo della tipologia selezionata","Navetta da/per aeroporto 24h","Vigilanza e custodia del mezzo 24h","Ripristino gratuito del calo di batteria della tua auto"]},"parkings":[{"id":123,"indoor":false,"insurance":false,"price":"13.56 EUR"},{"id":456,"indoor":false,"insurance":true,"price":"18.99 EUR"},{"id":243,"indoor":true,"insurance":true,"price":"23.56 EUR"},{"id":700,"indoor":true,"insurance":false,"price":"16.49 EUR"}],"dictionary":{"bookYourParking":"Prenota il tuo parcheggio a","featuresTitle":"Sono inclusi i seguenti servizi:","indoorSpace":"Posto auto coperto","outdoorSpace":"Posto auto scoperto","insuranceIncluded":"Assicurazione inclusa","insuranceExcluded":"Assicurazione esclusa"}});
+callback({
+   "vendor":{
+      "name":"ParkinGo",
+      "departureAirport":"Bergamo Orio al Serio",
+      "map":"http://lorempixel.com/370/390",
+      "features":[
+         "Parcheggio ParkinGo della tipologia selezionata",
+         "Navetta da/per aeroporto 24h",
+         "Vigilanza e custodia del mezzo 24h",
+         "Ripristino gratuito del calo di batteria della tua auto"
+      ]
+   },
+   "parkings":[
+      {
+         "id":123,
+         "indoor":false,
+         "insurance":false,
+         "price":"13.56 EUR",
+         "top":220,
+         "left":80
+      },
+      {
+         "id":456,
+         "indoor":false,
+         "insurance":true,
+         "price":"18.99 EUR",
+         "top":20,
+         "left":35
+      },
+      {
+         "id":243,
+         "indoor":true,
+         "insurance":true,
+         "price":"23.56 EUR",
+         "top":90,
+         "left":200
+      },
+      {
+         "id":700,
+         "indoor":true,
+         "insurance":false,
+         "price":"16.49 EUR",
+         "top":110,
+         "left":150
+      }
+   ],
+   "dictionary":{
+      "bookYourParking":"Prenota il tuo parcheggio a",
+      "featuresTitle":"Sono inclusi i seguenti servizi:",
+      "indoorSpace":"Posto auto coperto",
+      "outdoorSpace":"Posto auto scoperto",
+      "insuranceIncluded":"Assicurazione inclusa",
+      "insuranceExcluded":"Assicurazione esclusa",
+      "hideDetails":"nascondi dettagli",
+      "showDetails":"mostra dettagli",
+      "parkingDetails": "Dettagli Parcheggio",
+      "reserve": "prenota",
+      "reserved":"Posto prenotato con successo",
+      "loadingMap": "Caricamento mappa, attendere prego..."
+   }
+});
--- app/widget/parckingPlugin.js
+++ app/widget/parckingPlugin.js
@@ -1,3 +1,165 @@
+<<<<<<< HEAD
+function __comParckingPlugin(jQuery, jSelector, window, document) {
+  "use strict";
+  (function ($, jSelector, window, document) {
+    var baseAddress = "http://localhost:8080/widget/";
+    var data_jsonp_url = baseAddress + "assets/model_jsonp.js";
+    var html_url = baseAddress + "assets/widget.html";
+    var parking_dialoghtml_url = baseAddress + "assets/parking_dialog.html";
+    var style_url = baseAddress + "css/style.css";
+
+    var myData = { baseAddress: baseAddress };
+    var dialogHtml;
+    var dictionary;
+    $.fn.comParckingPlugin = function () {
+      var _self = this;
+
+      function loadCss() {
+        var css_link = jQuery("<link>", {
+          rel: "stylesheet",
+          type: "text/css",
+          href: style_url
+        });
+        css_link.appendTo('head');
+      }
+
+      function loadHtml(dataModel) {
+        $.get(html_url, function (remoteHtml) {
+          console.log("data html loaded");
+          dictionary = dataModel.dictionary;
+          praseData(dataModel, remoteHtml);
+        });
+
+        $.get(parking_dialoghtml_url, function (remoteHtml) {
+          console.log("parking data html loaded");
+          dialogHtml = remoteHtml;
+        });
+
+      }
+
+      function loadDataModel(callback) {
+        $.ajax({
+          url: data_jsonp_url,
+          dataType: 'jsonp',
+          jsonpCallback: 'callback',
+          success: function (data) {
+            console.log("Data successfully loaded");
+            callback(data);
+          },
+          error: function (xhr, status, errorThrown) {
+            console.error("ERROR getting data: ", xhr);
+            console.log(xhr);
+          }
+        });
+      }
+
+      function template_me(html, data) {
+
+        var old_var = _.templateSettings.variable;
+        _.templateSettings.variable = "com_parking";
+
+        var template = _.template(html);
+        var parsedHtml = template($.extend(myData, data));
+        _.templateSettings.variable = old_var;
+
+        return parsedHtml;
+      }
+
+      function praseData(dataModel, html) {
+        /*<img src="<%= com_parking.vendor.map %>"></img> */
+        var parsedHtml = template_me(html, dataModel);
+        _self.html(parsedHtml);
+        bindElements(dataModel);
+
+        var mapImage = new Image();
+        $(mapImage).load(function () {
+          $('.com_parking_widget_pakring_map_container', _self).html(mapImage)
+            .addClass("com_parking_widget_pakring_map");
+
+          $('.com_parking_widget_pakring_blocks', _self).show();
+
+        }).attr('src', dataModel.vendor.map).error(function () {
+          alert("Error loading map image");
+        });
+      }
+
+      function bindElements(dataModel) {
+        $(".com_parking_widget_footer .com_parking_widget_showdetails").click(function () {
+          toggleParkingDetail(dataModel);
+        });
+
+
+        $(".com_parking_widget_block_icon").click(function () {
+          var parking = JSON.parse($(this).attr("data-parking"));
+          showParkingBlockInfo(parking, $(this));
+        });
+      }
+
+      function closeDialog() {
+        $(".com_parking_widget_parking_dialog").hide();
+      }
+
+      function showParkingBlockInfo(parking, obj) {
+        console.log("clicked on ", parking);
+        var parsedHtml = template_me(dialogHtml, { selectedparking: parking, dictionary: dictionary });
+        $(".com_parking_widget_parking_dialog").html(parsedHtml);
+
+        var top = obj.css("top");
+        var left = obj.css("left");
+        $(".com_parking_widget_parking_dialog").css("top", top);
+        $(".com_parking_widget_parking_dialog").css("left", left);
+        $(".com_parking_widget_parking_dialog").show();
+        $(".com_parking_dialog_header a").click(function () {
+          closeDialog();
+        });
+
+        $(".com_parking_dialog_btn_reserve").click(function (e) {
+          var my_element = $(this)[0];
+
+          e.detail = parking;
+          triggerEvent(my_element, 'customChangeEvent', e);
+          closeDialog();
+
+        });
+      }
+
+      function triggerEvent(el, eventName, options) {
+        var event;
+        if (window.CustomEvent) {
+          event = new CustomEvent(eventName, options);
+        } else {
+          event = document.createEvent('CustomEvent');
+          event.initCustomEvent(eventName, true, true, options);
+        }
+        el.dispatchEvent(event);
+      }
+
+      function toggleParkingDetail(dataModel) {
+        $(".com_parking_widget_container").slideToggle("slow", function () {
+          if ($(".com_parking_widget_container").is(":visible") == true) {
+            $(".com_parking_widget_footer hr").show();
+            $(".com_parking_widget_footer .com_parking_widget_showdetails").html(dataModel.dictionary.hideDetails);
+          }
+          else {
+            $(".com_parking_widget_footer hr").hide();
+            $(".com_parking_widget_footer .com_parking_widget_showdetails").html(dataModel.dictionary.showDetails);
+          }
+        });
+      }
+
+      function initPlugin() {
+        loadCss();
+        loadDataModel(loadHtml);
+      }
+
+      initPlugin();
+
+      return this;
+    };
+
+    $(jSelector).comParckingPlugin();
+  } (jQuery, jSelector, window, document));
+=======
 function __comParckingPlugin(jQuery, jSelector) {
   "use strict";
   (function ($, jSelector) {
@@ -70,4 +232,5 @@ function __comParckingPlugin(jQuery, jSelector) {
 
     $(jSelector).comParckingPlugin();
   } (jQuery, jSelector));
+>>>>>>> 469d3db173dff89b3daaca7626fabb9991e8e09b
 }
