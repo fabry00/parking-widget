8721f9456ae14f0b92bf3d01c7694faeab99dc04
fabry <fabry_00@hotmail.com>
2016-08-03 18:39:52 +0200

    fixed conflicting files

--- app/widget/parckingPlugin.js
+++ app/widget/parckingPlugin.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 function __comParckingPlugin(jQuery, jSelector, window, document) {
   "use strict";
   (function ($, jSelector, window, document) {
@@ -79,7 +78,7 @@ function __comParckingPlugin(jQuery, jSelector, window, document) {
           $('.com_parking_widget_pakring_blocks', _self).show();
 
         }).attr('src', dataModel.vendor.map).error(function () {
-          alert("Error loading map image");
+          alert("Error loading map image from: "+dataModel.vendor.map+" retry please");
         });
       }
 
@@ -159,78 +158,5 @@ function __comParckingPlugin(jQuery, jSelector, window, document) {
 
     $(jSelector).comParckingPlugin();
   } (jQuery, jSelector, window, document));
-=======
-function __comParckingPlugin(jQuery, jSelector) {
-  "use strict";
-  (function ($, jSelector) {
-    var shade = "#556b2f";
-    var data_jsonp_url = "http://localhost:8080/widget/assets/model_jsonp.js";
-    // This also should be a JSONP request
-    var html_url = "http://localhost:8080/widget/assets/widget.html";
 
-    $.fn.comParckingPlugin = function () {
-      var _self = this;
-
-      function loadCss() {
-        var css_link = jQuery("<link>", {
-          rel: "stylesheet",
-          type: "text/css",
-          href: "http://localhost:8080/widget/css/style.css"
-        });
-        css_link.appendTo('head');
-      }
-
-      function loadHtml(dataModel) {
-        // This also should be a JSONP request 
-        $.get(html_url, function (remoteHtml) {
-          console.log("data html loaded");
-          praseData(dataModel, remoteHtml);
-        });
-      }
-
-      function loadDataModel(callback) {
-        $.ajax({
-          url: data_jsonp_url,
-          dataType: 'jsonp',
-          jsonpCallback: 'callback',
-          success: function (data) {
-            console.log("Data successfully loaded");
-            callback(data);
-          },
-          error: function (xhr, status, errorThrown) {
-            console.error("ERROR getting data: ", xhr);
-            console.log(xhr);
-          }
-        });
-      }
-
-      function template_me(html, data) {
-
-        var old_var = _.templateSettings.variable;
-        
-        _.templateSettings.variable = "com_parking";
-        var template = _.template(html);
-        _self.html(template(data));
-
-        _.templateSettings.variable = old_var;
-      }
-
-      function praseData(dataModel, html) {
-/*<img src="<%= com_parking.vendor.map %>"></img> */
-        template_me(html, dataModel);
-      }
-
-      function initPlugin() {
-        loadCss();
-        loadDataModel(loadHtml);
-      }
-
-      initPlugin();
-
-      return this;
-    };
-
-    $(jSelector).comParckingPlugin();
-  } (jQuery, jSelector));
->>>>>>> 469d3db173dff89b3daaca7626fabb9991e8e09b
 }
